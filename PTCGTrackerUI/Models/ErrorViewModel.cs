02baa06c44891d0817b877c320cabfa2f4f601aa
0eef0 <ethanryanroldan@gmail.com>
2026-08-16 13:54:27 -0700

    Created Initial Project for UI

---
+++ PTCGTrackerUI/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PTCGTrackerUI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
