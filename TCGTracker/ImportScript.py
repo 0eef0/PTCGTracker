57234e7debff17f826eea2289b77490b0c0397dc
SKoN123456 <msued5203@gmail.com>
2026-08-16 21:26:17 -0700

    Add files via upload

---
+++ TCGTracker/ImportScript.py
@@ -0,0 +1,125 @@
+from db import get_db_connection
+
+def deckUpload(text):
+    Categories = text.split("\n\n")
+
+    Regulation = {
+        "SVI": "G", "PAL": "G", "OBF": "G", "MEW":"G", "PAR": "G", "PAF": "G",
+        "TEF": "H", "TWM": "H", "SFA": "H", "SCR": "H", "SSP": "H", "PRE": "H",
+        "JTG": "I", "DRI": "I", "BLK": "I", "WHT": "I", "MEG": "I", "PFL": "I", "ASC": "I",
+        "POR": "J", "CRI": "J", "PBL": "J", "30C": "J",
+    }
+
+    #Pokemon
+    Pokemon = Categories[0]
+    Pokemon = Pokemon.split("\n")
+
+    PokemonInDeck = []
+
+    for card in Pokemon:
+        if card != Pokemon[0]:
+            card = card.split(" ")
+            index = 1
+            cardName = ""
+            while index < len(card)-2:
+                cardName += card[index] + " "
+                index += 1
+
+            if card[len(card)-2] in Regulation.keys():
+                regMark = Regulation[card[len(card)-2]]
+            else:
+                regMark = "Other"
+
+            PokemonInDeck.append({
+                "Name": cardName.strip(),
+                "Set": card[len(card)-2],
+                "Number": card[len(card)-1],
+                "QTYinDeck": card[0],
+                "Regulation": regMark
+            })
+
+    #Trainers
+    Trainers = Categories[1]
+    Trainers = Trainers.split("\n")
+
+    TrainerInDeck = []
+
+    for card in Trainers:
+        if card != Trainers[0]:
+            card = card.split(" ")
+            index = 1
+            cardName = ""
+            while index < len(card)-2:
+                cardName += card[index] + " "
+                index += 1
+
+            if card[len(card)-2] in Regulation.keys():
+                regMark = Regulation[card[len(card)-2]]
+            else:
+                regMark = "Other"
+
+            TrainerInDeck.append({
+                "Name": cardName.strip(),
+                "Set": card[len(card)-2],
+                "Number": card[len(card)-1],
+                "QTYinDeck": card[0],
+                "Regulation": regMark
+            })
+
+    #Energy
+    Energy = Categories[2]
+    Energy = Energy.split("\n")
+
+    EnergyInDeck = []
+
+    for card in Energy:
+        if card != Energy[0]:
+            card = card.split(" ")
+            index = 1
+            cardName = ""
+            while index < len(card)-2:
+                cardName += card[index] + " "
+                index += 1
+
+            if card[len(card)-2] in Regulation.keys():
+                regMark = Regulation[card[len(card)-2]]
+            else:
+                regMark = "Other"
+
+            EnergyInDeck.append({
+                "Name": cardName.strip(),
+                "Set": card[len(card)-2],
+                "Number": card[len(card)-1],
+                "QTYinDeck": card[0],
+                "Regulation": regMark
+            })
+
+    #Database
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    for card in PokemonInDeck:
+        cursor.execute("""INSERT INTO \"DeckCard\" (dc_deckid, dc_qtylist, dc_qtydeck, dc_name, dc_set, dc_reg, dc_type, dc_setnumber) 
+                           VALUES (%s, %s, %s, %s, %s, %s, %s, %s)""", (1, card["QTYinDeck"], 0, card["Name"], card["Set"], card["Regulation"], "Pokemon", card["Number"]))
+        conn.commit()
+
+    for card in TrainerInDeck:
+        cursor.execute("""INSERT INTO \"DeckCard\" (dc_deckid, dc_qtylist, dc_qtydeck, dc_name, dc_set, dc_reg, dc_type, dc_setnumber) 
+                           VALUES (%s, %s, %s, %s, %s, %s, %s, %s)""", (1, card["QTYinDeck"], 0, card["Name"], card["Set"], card["Regulation"], "Trainer", card["Number"]))
+        conn.commit()
+
+    for card in EnergyInDeck:
+        cursor.execute("""INSERT INTO \"DeckCard\" (dc_deckid, dc_qtylist, dc_qtydeck, dc_name, dc_set, dc_reg, dc_type, dc_setnumber) 
+                           VALUES (%s, %s, %s, %s, %s, %s, %s, %s)""", (1, card["QTYinDeck"], 0, card["Name"], card["Set"], card["Regulation"], "Energy", card["Number"]))
+        conn.commit()
+
+    cursor.close()
+    conn.close()
+
+    
+
+
+
+
+
+
