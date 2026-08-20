0d6bf25633d58638d6e241d4dc84ad0325e406e5
SKoN123456 <msued5203@gmail.com>
2026-08-19 20:59:13 -0700

    Add files via upload

--- TCGTracker/ImportScript.py
+++ TCGTracker/ImportScript.py
@@ -1,16 +1,16 @@
 from db import get_db_connection
 
-def deckUpload(text):
+def deckUpload(text, deckname, username):
     Categories = text.split("\n\n")
 
     Regulation = {
-        "SVI": "G", "PAL": "G", "OBF": "G", "MEW":"G", "PAR": "G", "PAF": "G",
+        "SVI": "G", "PAL": "G", "OBF": "G", "MEW": "G", "PAR": "G", "PAF": "G",
         "TEF": "H", "TWM": "H", "SFA": "H", "SCR": "H", "SSP": "H", "PRE": "H",
         "JTG": "I", "DRI": "I", "BLK": "I", "WHT": "I", "MEG": "I", "PFL": "I", "ASC": "I",
         "POR": "J", "CRI": "J", "PBL": "J", "30C": "J",
     }
 
-    #Pokemon
+    # Pokemon
     Pokemon = Categories[0]
     Pokemon = Pokemon.split("\n")
 
@@ -21,24 +21,24 @@ def deckUpload(text):
             card = card.split(" ")
             index = 1
             cardName = ""
-            while index < len(card)-2:
+            while index < len(card) - 2:
                 cardName += card[index] + " "
                 index += 1
 
-            if card[len(card)-2] in Regulation.keys():
-                regMark = Regulation[card[len(card)-2]]
+            if card[len(card) - 2] in Regulation.keys():
+                regMark = Regulation[card[len(card) - 2]]
             else:
                 regMark = "Other"
 
             PokemonInDeck.append({
                 "Name": cardName.strip(),
-                "Set": card[len(card)-2],
-                "Number": card[len(card)-1],
+                "Set": card[len(card) - 2],
+                "Number": card[len(card) - 1],
                 "QTYinDeck": card[0],
                 "Regulation": regMark
             })
 
-    #Trainers
+    # Trainers
     Trainers = Categories[1]
     Trainers = Trainers.split("\n")
 
@@ -49,24 +49,24 @@ def deckUpload(text):
             card = card.split(" ")
             index = 1
             cardName = ""
-            while index < len(card)-2:
+            while index < len(card) - 2:
                 cardName += card[index] + " "
                 index += 1
 
-            if card[len(card)-2] in Regulation.keys():
-                regMark = Regulation[card[len(card)-2]]
+            if card[len(card) - 2] in Regulation.keys():
+                regMark = Regulation[card[len(card) - 2]]
             else:
                 regMark = "Other"
 
             TrainerInDeck.append({
                 "Name": cardName.strip(),
-                "Set": card[len(card)-2],
-                "Number": card[len(card)-1],
+                "Set": card[len(card) - 2],
+                "Number": card[len(card) - 1],
                 "QTYinDeck": card[0],
                 "Regulation": regMark
             })
 
-    #Energy
+    # Energy
     Energy = Categories[2]
     Energy = Energy.split("\n")
 
@@ -77,46 +77,71 @@ def deckUpload(text):
             card = card.split(" ")
             index = 1
             cardName = ""
-            while index < len(card)-2:
+            while index < len(card) - 2:
                 cardName += card[index] + " "
                 index += 1
 
-            if card[len(card)-2] in Regulation.keys():
-                regMark = Regulation[card[len(card)-2]]
+            if card[len(card) - 2] in Regulation.keys():
+                regMark = Regulation[card[len(card) - 2]]
             else:
                 regMark = "Other"
 
             EnergyInDeck.append({
                 "Name": cardName.strip(),
-                "Set": card[len(card)-2],
-                "Number": card[len(card)-1],
+                "Set": card[len(card) - 2],
+                "Number": card[len(card) - 1],
                 "QTYinDeck": card[0],
                 "Regulation": regMark
             })
 
-    #Database
+    # Database
     conn = get_db_connection()
     cursor = conn.cursor()
 
+    cursor.execute("""SELECT * FROM \"User\" WHERE u_username = %s""", (username,))
+    userid = cursor.fetchone()[0]
+
+    cursor.execute("""SELECT d_version FROM \"Deck\" WHERE d_userid = %s AND d_name = %s""", (userid, deckname))
+    version = cursor.fetchone()
+
+    if version == None:
+        cursor.execute(
+            """INSERT INTO \"Deck\" (d_userid, d_name, d_wins, d_losses, d_version) VALUES (%s, %s, %s, %s, %s) RETURNING d_deckid""",
+            (userid, deckname, 0, 0, 1))
+        deckid = cursor.fetchone()[0]
+        conn.commit()
+    else:
+        cursor.execute(
+            """INSERT INTO \"Deck\" (d_userid, d_name, d_wins, d_losses, d_version) VALUES (%s, %s, %s, %s, %s) RETURNING d_deckid""",
+            (userid, deckname, 0, 0, version[0] + 1))
+        deckid = cursor.fetchone()[0]
+        conn.commit()
+
     for card in PokemonInDeck:
         cursor.execute("""INSERT INTO \"DeckCard\" (dc_deckid, dc_qtylist, dc_qtydeck, dc_name, dc_set, dc_reg, dc_type, dc_setnumber) 
-                           VALUES (%s, %s, %s, %s, %s, %s, %s, %s)""", (1, card["QTYinDeck"], 0, card["Name"], card["Set"], card["Regulation"], "Pokemon", card["Number"]))
+                           VALUES (%s, %s, %s, %s, %s, %s, %s, %s)""",
+                       (deckid, card["QTYinDeck"], 0, card["Name"], card["Set"], card["Regulation"], "Pokemon",
+                        card["Number"]))
         conn.commit()
 
     for card in TrainerInDeck:
         cursor.execute("""INSERT INTO \"DeckCard\" (dc_deckid, dc_qtylist, dc_qtydeck, dc_name, dc_set, dc_reg, dc_type, dc_setnumber) 
-                           VALUES (%s, %s, %s, %s, %s, %s, %s, %s)""", (1, card["QTYinDeck"], 0, card["Name"], card["Set"], card["Regulation"], "Trainer", card["Number"]))
+                           VALUES (%s, %s, %s, %s, %s, %s, %s, %s)""",
+                       (deckid, card["QTYinDeck"], 0, card["Name"], card["Set"], card["Regulation"], "Trainer",
+                        card["Number"]))
         conn.commit()
 
     for card in EnergyInDeck:
         cursor.execute("""INSERT INTO \"DeckCard\" (dc_deckid, dc_qtylist, dc_qtydeck, dc_name, dc_set, dc_reg, dc_type, dc_setnumber) 
-                           VALUES (%s, %s, %s, %s, %s, %s, %s, %s)""", (1, card["QTYinDeck"], 0, card["Name"], card["Set"], card["Regulation"], "Energy", card["Number"]))
+                           VALUES (%s, %s, %s, %s, %s, %s, %s, %s)""",
+                       (deckid, card["QTYinDeck"], 0, card["Name"], card["Set"], card["Regulation"], "Energy",
+                        card["Number"]))
         conn.commit()
 
     cursor.close()
     conn.close()
 
-    
+
 
 
 
--- TCGTracker/discordbot.py
+++ TCGTracker/discordbot.py
@@ -4,6 +4,8 @@ import logging
 from dotenv import load_dotenv
 import os
 from ImportScript import *
+from db import get_db_connection
+import random
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
@@ -18,10 +20,142 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 @bot.event
 async def on_ready():
     print(f"We are ready to go in, {bot.user.name}")
+    synced = await bot.tree.sync()
+    print(f"Synced {len(synced)} commands:")
 
-@bot.command()
-async def test(ctx, *, message: str):
-    deckUpload(message)
-    await ctx.send("The Deck has been Submitted to the database!")
+    for command in synced:
+        print(
+            f"Command: /{command.name} | "
+            f"Description: {command.description}"
+        )
+
+@bot.tree.command(name='poem', description='Sends a beautiful poem')
+async def poem(interaction: discord.Interaction):
+    await interaction.response.send_message('''Monkeys can climb
+Crickets can leap
+Horses can race
+Owls can seek
+Cheetahs can run
+Eagles can fly
+People can try
+But that's about it.''')
+
+@bot.tree.command(name='link', description='Link to tracker UI')
+async def poem(interaction: discord.Interaction):
+    await interaction.response.send_message('Link goes here')
+
+class DecklistModal(discord.ui.Modal, title="Upload Decklist"):
+
+    deckname = discord.ui.TextInput(
+        label="Deck Name",
+        placeholder="Name your deck...",
+        style = discord.TextStyle.paragraph,
+        required=True,
+    )
+
+    decklist = discord.ui.TextInput(
+        label="Decklist",
+        placeholder="Paste your decklist here...",
+        style = discord.TextStyle.paragraph,
+        required=True,
+        max_length=4000
+    )
+
+    async def on_submit(self, interaction: discord.Interaction):
+        await interaction.response.defer()
+        decklist_text = self.decklist.value
+        deckname_text = self.deckname.value
+        username = interaction.user.name
+        deckUpload(decklist_text, deckname_text, username)
+        await interaction.followup.send(
+            f"Your {deckname_text} deck has been submitted to the database!",  ephemeral=True
+        )
+
+@bot.tree.command(name="upload", description="Upload a decklist to the database")
+async def uploaddecklist(interaction: discord.Interaction):
+    await interaction.response.send_modal(DecklistModal())
+
+@bot.tree.command(name="delete", description="Delete a decklist from the database")
+async def uploaddecklist(interaction: discord.Interaction, deckname: str, version: str):
+    username = interaction.user.name
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("""DELETE FROM \"Deck\" USING \"User\" WHERE u_username = %s AND d_name = %s AND d_version = %s""", (username, deckname, version,))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    await interaction.response.send_message(f"Your {deckname} deck has been removed from the database!", ephemeral=True)
+
+@bot.tree.command(name="mydecks", description="Get a list of all your decks and their stats")
+async def mydecks(interaction: discord.Interaction):
+    username = interaction.user.name
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("""SELECT * FROM \"Deck\" JOIN \"User\" ON u_userid = d_userid WHERE u_username = %s""", (username,))
+    decks = cursor.fetchall()
+    cursor.close()
+    conn.close()
+
+    deckstring = ""
+    for deck in decks:
+        deckstring += f"{deck[2]}-v{deck[5]}.0 | {deck[3]}/{deck[4]} W/L\n "
+
+    await interaction.response.send_message(f"{deckstring}",  ephemeral=True)
+
+@bot.tree.command(name="randomdeck", description="Get a random deck from your list to play")
+async def randomdeck(interaction: discord.Interaction):
+    username = interaction.user.name
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("""SELECT * FROM \"Deck\" JOIN \"User\" ON u_userid = d_userid WHERE u_username = %s""", (username,))
+    decks = cursor.fetchall()
+    cursor.close()
+    conn.close()
+
+    randomselect = random.choice(decks)
+    await interaction.response.send_message(f"Your randomly selected deck is: {randomselect[2]}!",  ephemeral=True)
+
+@bot.tree.command(name="cardstats", description="See the usage stats of a specific card")
+async def cardstats(interaction: discord.Interaction, cardname: str):
+    username = interaction.user.name
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("""SELECT d_deckid, d_name, d_version, dc_name, dc_set, dc_setnumber, dc_qtylist, dc_qtydeck FROM \"DeckCard\"
+                JOIN \"Deck\" on d_deckid = dc_deckid JOIN \"User\" ON u_userid = d_userid WHERE u_username = %s AND dc_name LIKE %s
+                """, (username, f"%{cardname}%", ))
+    cards = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    cardstatstring = ""
+
+    for card in cards:
+        cardstatstring += f"{card[1]}-v{card[2]}.0: {card[3]} ({card[4]} - {card[5]}) | {card[6]} list - {card[7]} physical\n"
+
+    if cardstatstring:
+        await interaction.response.send_message(f"{cardstatstring}", ephemeral=True)
+    else:
+        await interaction.response.send_message(f"Sorry, Requested card was not found", ephemeral=True)
+
+@bot.tree.command(name="register", description="Register User in database")
+async def newuser(interaction: discord.Interaction):
+    username = interaction.user.name
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("""INSERT INTO \"User\" (u_username) VALUES (%s)""", (username,))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    await interaction.response.send_message("You have been registered to the database!", ephemeral=True)
+
+@bot.tree.command(name="remove", description="Remove User from database")
+async def deleteuser(interaction: discord.Interaction):
+    username = interaction.user.name
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("""DELETE FROM \"User\" WHERE u_username = %s""", (username,))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    await interaction.response.send_message("You have been removed from the database!",  ephemeral=True)
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
